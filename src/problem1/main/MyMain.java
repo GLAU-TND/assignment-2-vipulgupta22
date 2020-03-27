5ff40d0ac28d035e0b16e4dac4fd9ec59c24d8cc
vipul <vipultagore@gmail.com>
2020-03-27 16:09:18 +0530

    problem ho gyi

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -5,7 +5,21 @@
  *  Time: 7:17 PM
  */
 package problem1.main;
+
+import problem1.mybst.MyBinarySearchTree;
+
+import java.util.Scanner;
 // executable class
 
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+        Scanner scanner = new Scanner(System.in);
+        // taking only 7 inputs for simple demonstration with value 1,2,3,4,5,6,7
+        int i = 7;
+        while (i > 0) {
+            tree.add(scanner.nextInt());
+            i--;
+        }
+    }
 }
